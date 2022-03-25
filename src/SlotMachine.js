05b16863340e1fba774b9e0a84186bf694fcc57e
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-25 12:48:37 -0600

    Creating fake props

--- src/SlotMachine.js
+++ src/SlotMachine.js
@@ -1,8 +1,11 @@
+import Lottery from './Lottery';
+
 const SlotMachine = () => {
     return ( 
 
         <>
-        I am the slot SlotMachine, pick stuff
+        <Lottery title ="Mega" balls="6" values="40" />
+        <Lottery title="Mini" balls="6" values="40" />
         </>
      );
 }
