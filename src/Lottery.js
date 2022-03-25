78c75475e01362f4019fd2bb0546fbf2452bb19c
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-25 12:52:11 -0600

    Creating base useStates

--- src/Lottery.js
+++ src/Lottery.js
@@ -1,9 +1,19 @@
+import React, {useState} from 'react';
+
 const Lottery = ({title, balls, values}) => {
+
+    const [numbers, setNumbers] = useState('');
+
+const handleRoll = () => {
+console.log('Hello')
+}
     return ( 
         <>
         Lottery: {title}
         total balls: {balls}
         total values: {values}
+
+        <button onClick={handleRoll}>Generate New Numbers</button>
         </>
      );
 }
